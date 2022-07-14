aa0562dec485745289fbb1b0df1db1e01b94ac37
Darya <97470059+DaryaBobur@users.noreply.github.com>
2022-07-14 20:44:29 +0300

    Adds a solution to the 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const btnStart = document.querySelector(`button[data-start]`);
+const btnStop = document.querySelector(`button[data-stop]`);
+const bodyEl = document.querySelector('body');
+btnStart.addEventListener('click', colorChange);
+btnStop.addEventListener('click', stopColorChange);
+
+let timeChangeColor = null;
+
+btnStop.disabled = true;
+
+function colorChange() {
+  timeChangeColor = setInterval(() => {
+      bodyEl.style.backgroundColor = `${getRandomHexColor()}`; 
+  }, 1000)
+
+    btnStart.disabled = true
+    btnStop.disabled = false;
+}
+
+function stopColorChange() {
+  clearInterval(timeChangeColor);
+  
+  btnStart.disabled = false
+  btnStop.disabled = true
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
 
