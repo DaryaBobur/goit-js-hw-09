62ea95be83775dc6c3aaedfe7cde65361d53ebe4
Darya <97470059+DaryaBobur@users.noreply.github.com>
2022-07-17 23:12:57 +0300

    Adds a solution to the task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+const form = document.querySelector('form');
+
+form.addEventListener('submit', promisesData);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+  });
+};
+
+function promisesData(e) {
+  e.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+formValue(+delay.value, +step.value, +amount.value)
   }
+
+  function formValue(delay, step, amount) {
+    for (let position = 1; position <= amount; position += 1) {
+      createPromise(position, delay)
+        .then(({ position, delay }) => {
+          setTimeout(() => {
+            Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+          }, delay);
+        }).catch(({ position, delay }) => {
+          setTimeout(() => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+          }, delay);
+      
+        })
+      delay += step;
+    }
 }
