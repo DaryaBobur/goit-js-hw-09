6fe57725a343fcec92fe8efd6043c6e401d45b76
Darya <97470059+DaryaBobur@users.noreply.github.com>
2022-07-18 16:20:34 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,11 +6,13 @@ form.addEventListener('submit', promisesData);
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
-      if (shouldResolve) {
+    setTimeout(() => {
+   if (shouldResolve) {
         resolve({ position, delay });
       } else {
         reject({ position, delay });
       }
+    }, delay)
   });
 };
 
@@ -19,21 +21,18 @@ function promisesData(e) {
   const {
     elements: { delay, step, amount },
   } = e.currentTarget;
-formValue(+delay.value, +step.value, +amount.value)
+
+  formValue(+delay.value, +step.value, +amount.value)
   }
 
   function formValue(delay, step, amount) {
     for (let position = 1; position <= amount; position += 1) {
       createPromise(position, delay)
         .then(({ position, delay }) => {
-          setTimeout(() => {
-            Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
-          }, delay);
-        }).catch(({ position, delay }) => {
-          setTimeout(() => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+        })
+        .catch(({ position, delay }) => {
           Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-          }, delay);
-      
         })
       delay += step;
     }
